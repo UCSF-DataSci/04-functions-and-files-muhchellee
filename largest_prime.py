4f7887a01399bec800628c6f64f3a8ec3231b9a8
Michelle <181282244+muhchellee@users.noreply.github.com>
2024-10-09 11:20:07 -0700

    Added largest prime fibonacci number finder

--- largest_prime.py
+++ largest_prime.py
@@ -11,4 +11,40 @@ Write a program that takes a number as an argument, finds the *Fibonacci* number
 Task: Find the largest prime Fibonacci number less that 50000
 """
 
-# You're on your own for this one. Good luck!
+import argparse
+from fibonnaci import generate_fibonacci
+
+### function to check if a number is prime ###
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+### function to find largest prime Fibonacci number less than specified limit ###
+def largest_prime_fibonacci(limit):
+    fibonacci_nums = generate_fibonacci(limit)
+    
+    prime_fibonacci_nums = [num for num in fibonacci_nums if is_prime(num)]
+    
+    if prime_fibonacci_nums:
+        return max(prime_fibonacci_nums)
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    # setting up command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('limit', type=int, help="Upper limit for Fibonacci numbers")
+    
+    args = parser.parse_args()
+    
+    output_num = largest_prime_fibonacci(args.limit)
+    
+    print(output_num)
+
+# largest prime fibonacci number less than 50000 is 28657
